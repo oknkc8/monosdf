b7ec9585692213f1908efee0c8061c92de0404f0
oknkc8 <oknkc8@gmail.com>
2022-11-01 23:17:49 +0900

    update

---
+++ code/run_dtu_mlp.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=0 \
+python -m torch.distributed.launch \
+--master_port 29499 \
+--nproc_per_node 1 \
+--nnodes=1 \
+--node_rank=0 \
+training/exp_runner.py \
+--conf confs/dtu_mlp_3views_reg.conf  \
+--scan_id 83
